7afc6d162a0275899913ff9c2ea2f97b89e81df1
Mark Karavan <mkaravan@Marks-MacBook-Pro.local>
2021-12-27 00:25:42 -0500

    Adds block scoping

--- environment.js
+++ environment.js
@@ -7,8 +7,9 @@ class Environment {
   /**
   *   Creates an Environment
   */
-  constructor(record = {}){
+  constructor(record = {}, parent = null) {
     this.record = record;
+    this.parent = parent;
   }
 
   /**
@@ -19,11 +20,36 @@ class Environment {
     return value;
   }
 
+  /**
+  *   Updates an existing variable
+  */
+  assign(name, value){
+    this._resolve(name).record[name] = value;
+    return value;
+  }
+
+  /**
+  *   Returns the value of a defined variable, or throws
+  *   if the variable is not defined.
+  */
   lookup(name) {
-    if (!this.record.hasOwnProperty(name)) {
+    return this._resolve(name).record[name];
+  }
+
+  /**
+  *   Returns a specific environment in which a variables is defined, or
+  *   throws if a variable is not defined
+  */
+  _resolve(name) {
+    if (this.record.hasOwnProperty(name)) {
+      return this;
+    }
+
+    if (this.parent == null) {
       throw new ReferenceError(`Variable "${name}" is not defined.`);
     }
-    return this.record[name];
+
+    return this.parent._resolve(name);
   }
 }
 
--- eva.js
+++ eva.js
@@ -16,29 +16,36 @@ class Eva {
   eval(exp, env = this.global) {
     // -------------------------------------
     // Self-evaluating expressions:
-    if (isNumber(exp)) {
+    if (isNumber(exp, env)) {
       return exp;
     }
 
-    if (isString(exp)) {
+    if (isString(exp, env)) {
       return exp.slice(1, -1);
     }
 
     // -------------------------------------
     // Math operations:
     if (exp[0] === '+') {
-      return this.eval(exp[1]) + this.eval(exp[2]);
+      return this.eval(exp[1], env) + this.eval(exp[2], env);
     }
 
     if (exp[0] === '*') {
-      return this.eval(exp[1]) * this.eval(exp[2]);
+      return this.eval(exp[1], env) * this.eval(exp[2], env);
     }
 
     // -------------------------------------
     // Variable declaration:
     if (exp[0] === 'var') {
       const [_, name, value] = exp;
-      return env.define(name, this.eval(value));
+      return env.define(name, this.eval(value, env));
+    }
+
+    // -------------------------------------
+    // Variable update:
+    if (exp[0] === 'set') {
+      const [_, name, value] = exp;
+      return env.assign(name, this.eval(value, env));
     }
 
     // -------------------------------------
@@ -47,9 +54,28 @@ class Eva {
       return env.lookup(exp);
     }
 
+    // -------------------------------------
+    // Blocks:
+    if (exp[0] === 'begin') {
+      const blockEnv = new Environment({}, env);
+      return this._evalBlock(exp, blockEnv);
+    }
+
 
 
     throw `Unimplemented: ${JSON.stringify(exp)}`;
+  }
+
+  _evalBlock(block, env) {
+    let result;
+
+    const [_tag, ...expressions] = block;
+
+    expressions.forEach(exp => {
+      result = this.eval(exp, env);
+    });
+
+    return result;
   }
 }
 
@@ -93,5 +119,47 @@ assert.strictEqual(eva.eval('VERSION'), '0.1');
 assert.strictEqual(eva.eval(['var', 'isUser', 'true']), true);
 assert.strictEqual(eva.eval(['var', 'z', ['*', 2, 3]]), 6);
 assert.strictEqual(eva.eval('z'), 6);
+
+// Blocks
+assert.strictEqual(eva.eval(
+  ['begin',
+    ['var', 'x', 10],
+    ['var', 'y', 20],
+    ['+', ['*', 'x', 'y'], 30]
+  ]),
+230);
+
+assert.strictEqual(eva.eval(
+  ['begin',
+    ['var', 'x', 10],
+    ['begin',
+      ['var', 'x', 20],
+      'x'
+    ],
+    'x'
+  ]),
+10);
+
+assert.strictEqual(eva.eval(
+  ['begin',
+    ['var', 'value', 10],
+    ['var', 'result',
+      ['begin',
+      ['var', 'x', ['+', 'value', 10]],
+      'x'
+    ]],
+    'result'
+  ]),
+20);
+
+assert.strictEqual(eva.eval(
+  ['begin',
+    ['var', 'data', 10],
+    ['begin',
+      ['set', 'data', 100],
+    ],
+    'data'
+  ]),
+100);
 
 console.log('All assertions passed!');
