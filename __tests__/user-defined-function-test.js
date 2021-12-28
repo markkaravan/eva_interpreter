3b136bd5f00a7f3dcc6c85ae53b2d47bd502adc8
Mark Karavan <mkaravan@Marks-MacBook-Pro.local>
2021-12-27 21:58:21 -0500

    Recursive functions work

--- __tests__/user-defined-function-test.js
+++ __tests__/user-defined-function-test.js
@@ -56,4 +56,17 @@ module.exports = eva => {
 
     `,
   160);
+
+  // Recursive function:
+  test(eva, `
+    (begin
+      (def factorial (x)
+        (if (= x 1)
+          1
+          (* x (factorial (- x 1)))
+      ))
+      (factorial 5)
+    )
+    `,
+  120);
 }
